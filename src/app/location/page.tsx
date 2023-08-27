698042d5bde7782733a5e951d922fc827f487707
Amirreza <amirreza.m13825@gmail.com>
2023-08-27 19:05:25 +0330

    implement loading and update API in the location page

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,9 +1,12 @@
 "use client";
+import { Card } from "@/components/Card";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import LoadingPage from "../loading";
 
 export default function LocationPage() {
   const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
   const searchParams = useSearchParams();
@@ -12,12 +15,19 @@ export default function LocationPage() {
   useEffect(() => {
     if (search) {
       fetch(
-        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${search}`
+        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${search}`
       )
         .then((response) => response.json())
-        .then((json) => setData(json));
+        .then((json) => {
+          setLoading(false);
+          setData(json);
+        });
     }
   }, []);
 
-  return <></>;
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  // return <Card data={data} />;
 }
