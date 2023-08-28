812b8d5b81ad189bdcf3b3ab25f14555cee8299b
Amirreza <amirreza.m13825@gmail.com>
2023-08-28 18:46:24 +0330

    develop the functionality of the card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,6 +7,13 @@ interface CardProps {
 }
 
 export function Card({ data }: CardProps) {
+  const date = new Date();
+  const dayName = date.toLocaleString("en-US", { weekday: "long" });
+  const formattedDate = date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+  });
+
   return (
     <div className="sm:flex sm:items-center sm:justify-center h-full">
       <div className="flex flex-col gap-y-8 items-center justify-center p-8 sm:flex-row">
@@ -14,17 +21,22 @@ export function Card({ data }: CardProps) {
         <div className="bg-gradient-to-tl from-slate-900 to-slate-500 rounded-xl sm:translate-x-4">
           <div className="min-w-[18rem] sm:w-96 flex flex-col justify-between gap-y-8 p-6">
             <div className="flex flex-col gap-y-2">
-              <h2 className="font-semibold text-3xl">Sunday</h2>
-              <span className="text-lg">22 march</span>
+              <h2 className="font-semibold text-3xl">{dayName}</h2>
+              <span className="text-lg">{formattedDate}</span>
               <span className="flex items-center gap-x-1 text-lg">
                 <BiCurrentLocation />
-                <span>Iran</span>
+                <span>
+                  {data.location.name}, {data.location.country}
+                </span>
               </span>
             </div>
             <div className="flex flex-col gap-y-2">
-              <span>icon</span>
-              <span className="font-bold text-2xl">29 c</span>
-              <span className="font-bold text-2xl">sunny</span>
+              <span className="font-bold text-2xl">
+                {data.current.temp_c} &#8451;
+              </span>
+              <span className="font-bold text-2xl">
+                {data.current.condition.text}
+              </span>
             </div>
           </div>
         </div>
@@ -33,32 +45,28 @@ export function Card({ data }: CardProps) {
           {/* detail section */}
           <div className="text-xl font-semibold flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
-              <h2>PRECIPITATION</h2>
-              <span>0%</span>
+              <h2>VISIBILITY</h2>
+              <span>{data.current.vis_km} KM</span>
             </div>
             <div className="flex items-center justify-between">
               <h2>HUMIDITY</h2>
-              <span>42%</span>
-            </div>
-            <div className="flex items-center justify-between">
-              <h2>WIND</h2>
-              <span>3 km/h</span>
+              <span>{data.current.humidity}%</span>
             </div>
             <div className="flex items-center justify-between">
               <h2>MAX TEMP</h2>
-              <span>3 km/h</span>
+              <span>{data.forecast.forecastday[0].day.maxtemp_c} &#8451;</span>
             </div>
             <div className="flex items-center justify-between">
               <h2>MIN TEMP</h2>
-              <span>3 km/h</span>
+              <span>{data.forecast.forecastday[0].day.mintemp_c} &#8451;</span>
             </div>
             <div className="flex items-center justify-between">
               <h2>SUNRISE</h2>
-              <span>3 km/h</span>
+              <span>{data.forecast.forecastday[0].astro.sunrise}</span>
             </div>
             <div className="flex items-center justify-between">
               <h2>SUNSET</h2>
-              <span>3 km/h</span>
+              <span>{data.forecast.forecastday[0].astro.sunset}</span>
             </div>
           </div>
           {/* button */}
